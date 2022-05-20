a23972b06cab78ab7b63df4d293a60e77048b5da
Deepthi-SP <100423337+Deepthi-SP@users.noreply.github.com>
2022-05-20 15:38:07 +0530

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -19,3 +19,25 @@ else
 echo "Enter argument"
 fi
 
+OR
+
+if [ $# -ge 1 ]
+then
+   if [ $# -eq 2 ]
+   then
+	chd=$2
+   else
+	chd=`cd .`
+   fi
+   file=`find $chd -iname $1`
+   if [ `echo $file | wc -c` -gt 1 ]
+   then
+     number=`ls -inum $file | grep -o ^[0-9]*`
+     cd $chd
+     find $chd -inum $number
+   else
+     echo "No such file"
+  fi
+else
+echo "Enter argument"
+fi
